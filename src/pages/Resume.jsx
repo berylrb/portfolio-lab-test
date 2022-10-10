0afa3c7d3990b4b07cffd6e62ea5e9d117fd0185
Beryl B <beriebaldwin@gmail.com>
2022-10-10 13:46:38 -0400

    style resume page

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,9 +1,28 @@
+import styles from './Resume.module.css'
+import NavBar from '../elements/NavBar'
+import Cv from '../assets/cv.png'
+
 
 function Resume() {
+  const onButtonClick = () => {
+    fetch('Portfolio-Resume-Beryl-Baldwin.pdf').then(response => {
+        response.blob().then(blob => {
+            const fileURL = window.URL.createObjectURL(blob);
+            let alink = document.createElement('a');
+            alink.href = fileURL;
+            alink.download = 'Portfolio-Resume-Beryl-Baldwin.pdf';
+            alink.click();
+        })
+    })
+}
   return (
     <>
-      <h1>Resume</h1>
-      <h4><a href='/Portfolio-Beryl-Baldwin-Resume.pdf' download>Download my resume</a></h4>
+      <NavBar />
+      <div className={styles.bodyDiv}>
+          <img onClick={onButtonClick} className={styles.navIcon} src={Cv} alt='cv icon button link' />
+        <h1>Resume</h1>
+        <h4>Click the icon to download my resume</h4>
+      </div>
     </>
   )
 }
