60eb20dd5a1996cd3894f13b1ea5630a7a587424
Beryl B <beriebaldwin@gmail.com>
2022-10-10 14:14:19 -0400

    add emojis to about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,12 +5,12 @@ import NavBar from '../elements/NavBar';
 function About() {
   return (
     <>
-    <NavBar />
+      <NavBar />
       <div className={styles.aboutDiv}>
         <div className={styles.imgText}>
           <img src={Headshot} alt='headshot' className={styles.imgClass} />
           <div className={styles.p}>
-          <h2 className={styles.h1}>About Beryl</h2>
+            <h2 className={styles.h1}>About Beryl</h2>
             <p>I'm Beryl, a fullstack software engineer and startup founder who loves creating animations, illustrations, and interactive digital products that help solve real world problems-- no matter how small.
               <br />
               <br />
@@ -19,6 +19,45 @@ function About() {
               <br />
               I hold a BA in Psychology and Certificate of Innovation & Entrepreneurship from Duke University, as well as a Software Engineering Immersive Certificate from General Assembly.</p>
           </div>
+        </div>
+        <div className={styles.interestsDiv}>
+          <h3>I'm also a...</h3>
+
+          <ul className={styles.interests}>
+
+            <li className={styles.emojiP}>
+              <p>👾</p>
+              <p>gamer</p>
+            </li>
+            <li className={styles.emojiP}>
+              <p>🐼</p>
+              <p>animal lover</p>
+            </li>
+            <li className={styles.emojiP}>
+              <p>🏐</p>
+              <p>volleyball player</p>
+            </li>
+            <li className={styles.emojiP}>
+              <p>🏳️‍🌈</p>
+              <p>ally</p>
+            </li>
+            <li className={styles.emojiP}>
+              <p>🏔️</p>
+              <p>traveller</p>
+            </li>
+            <li className={styles.emojiP}>
+              <p>🎤</p>
+              <p>musician</p>
+            </li>
+            <li className={styles.emojiP}>
+              <p>🎨</p>
+              <p>artist</p>
+            </li>
+            <li className={styles.emojiP}>
+              <p>👩🏽‍🍳</p>
+              <p>wannabe chef</p>
+            </li>
+          </ul>
         </div>
       </div>
     </>
